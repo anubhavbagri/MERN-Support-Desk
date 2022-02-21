11690eaaaf70b0f1f6307b58ae00dcb4c6cd56bb
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-02-21 11:58:13 +0530

    feat: basic express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log('Server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+// Initialize app variable
+const app = express();
+
+app.get('/', (req, res) => {
+  //   res.send('Hello');
+
+  // if you want to send json
+  res.json({ message: 'Welcome to the Support Desk API' });
+
+  // if you want to send the status along with json
+  //   res.status(201).json({ message: 'Welcome to the Support Desk API' });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
