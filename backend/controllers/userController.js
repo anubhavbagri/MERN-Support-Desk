e7b31b84fed738f5fdc8b3eb49fdf08522ed1a82
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-02-22 00:34:02 +0530

    feat: protect routes & authentication api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -69,8 +69,18 @@ const loginUser = asyncHandler(async (req, res) => {
     res.status(401);
     throw new Error('Invalid credentials');
   }
+});
 
-  res.send('Login Route');
+// @desc Get current user
+// @route /api/users/me
+// @access Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(user);
 });
 
 // Generate token
@@ -89,4 +99,5 @@ const generateToken = (id) => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/userController');
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require('../controllers/userController');
+
+const { protect } = require('../middleware/authMiddleware');
 
 // post request to 'api/users' would mean posting or adding a user which is what 'registering' does
 router.post('/', registerUser);
 
 // Login just validates the user
 router.post('/login', loginUser);
+
+router.get('/me', protect, getMe);
 
 module.exports = router;
