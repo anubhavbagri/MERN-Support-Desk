fbe9e2ffc5227e7996c2d4d2f7b05bddb91b37f2
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-02-21 13:37:39 +0530

    fix: error handling + asyncHandler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,26 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc Register a new user
 // @route /api/users
 // @access public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
   // Validation
   if (!name || !email || !password) {
-    return res.status(400);
+    res.status(400);
     throw new Error('Please include all fields');
   }
 
   res.send('Register Route');
-};
+});
 
 // @desc Login a user
 // @route /api/users/login
 // @access public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.send('Login Route');
-};
+});
 
 module.exports = {
   registerUser,
