444df34d25e50ae5708c7e000fe669f9a82f38d7
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-02-21 12:20:06 +0530

    feat: functionality in controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { registerUser, loginUser } = require('../controllers/userController');
 
 // post request to 'api/users' would mean posting or adding a user which is what 'registering' does
-router.post('/', (req, res) => {
-  res.send('Register Route');
-});
+router.post('/', registerUser);
 
 // Login just validates the user
-router.post('/login', (req, res) => {
-  res.send('Login Route');
-});
+router.post('/login', loginUser);
 
 module.exports = router;
