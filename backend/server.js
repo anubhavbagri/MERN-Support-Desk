687e638766465b74af487bf0825b115bf545323b
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-02-21 13:29:40 +0530

    feat: creating custom error handler

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 5000;
 
 // Initialize app variable
@@ -15,5 +16,8 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+// middleware for error handler
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
