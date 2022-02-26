b9e0f0b05d06d7ec72db664b71ebed03a2bab3c9
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-02-26 11:07:24 +0530

    redundant

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-  })
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
 } else {
   app.get('/', (req, res) => {
     res.status(200).json({ message: 'Welcome to the Support Desk API' });
